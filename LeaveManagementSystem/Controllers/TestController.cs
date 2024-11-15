3e229e382a3625933018fd813a42807eabfaae6a
PARKJ4 <PARKJ4@E088351>
2024-11-15 14:17:19 -0500

    Add project files.

---
+++ LeaveManagementSystem/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Jaeri Park",
+                DateOfBirth = new DateTime(1990,09,30),
+            };
+            return View(data);
+        }
+    }
+}
